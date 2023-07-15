cf972fe9dcadb507a0ce6c49e2b863eb8d5018fc
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2023-07-15 23:12:46 +0200

    feat: add more than one key

--- server.py
+++ server.py
@@ -16,14 +16,33 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 MAX_TOKENS_PER_MINUTE = 12 * 10**4
 MAX_TOKENS_PER_REQUEST = 50
 # DELAY_BETWEEN_REQUESTS = 60.0 / (MAX_TOKENS_PER_MINUTE / MAX_TOKENS_PER_REQUEST)
-DELAY_BETWEEN_REQUESTS = 0.02
+DELAY_BETWEEN_REQUESTS = 0.03
 
 app = FastAPI()
 queue = asyncio.Queue()
-last_request_time = datetime.now()
-has_ratelimit_error = False
 
 results = {}
+
+
+class ApiKey:
+    def __init__(self, key):
+        self.key = key
+        self.last_request_time = datetime.now()
+        self.has_ratelimit_error = False
+
+
+class ApiKeysManager:
+    def __init__(self, api_keys):
+        self.api_keys = [ApiKey(key) for key in api_keys]
+
+    def get_next_key(self):
+        return min([api_key for api_key in self.api_keys if not api_key.has_ratelimit_error], key=lambda k: k.last_request_time)
+        
+
+api_keys_string = os.getenv("OPENAI_API_KEYS")
+api_keys_list = api_keys_string.split(', ')
+
+api_keys_manager = ApiKeysManager(api_keys_list)
 
 
 class Item(BaseModel):
@@ -31,14 +50,11 @@ class Item(BaseModel):
     max_waiting_time: float = 5
 
 
-async def send_message_to_openai(request: Dict, task_id: str, max_waiting_time: float):
-    global last_request_time
-    last_request_time = datetime.now()
-    asyncio.create_task(process_request(request, task_id))
+async def send_message_to_openai(request: Dict, task_id: str, max_waiting_time: float, api_key: ApiKey):
+    asyncio.create_task(process_request(request, task_id, max_waiting_time, api_key))
 
 
-async def process_request(request: Dict, task_id: str, max_waiting_time: float):
-    global has_ratelimit_error
+async def process_request(request: Dict, task_id: str, max_waiting_time: float, api_key: ApiKey):
     try:
         logging.info("started generating")
         completion = await asyncio.wait_for(openai.ChatCompletion.acreate(**request), timeout=max_waiting_time)
@@ -46,7 +62,7 @@ async def process_request(request: Dict, task_id: str, max_waiting_time: float):
         results[task_id] = completion.choices[0].message['content']
     except Exception as e:
         logging.info(f"Error: {e}")
-        has_ratelimit_error = True
+        api_key.has_ratelimit_error = True
         queue.put_nowait({"item": Item(request=request), "task_id": task_id})
         
 
@@ -71,22 +87,26 @@ async def startup_event():
 
 
 async def worker():
-    while True:
-        global last_request_time, has_ratelimit_error
-        print(DELAY_BETWEEN_REQUESTS)
+    global last_request_time, has_ratelimit_error
 
-        wait = max(DELAY_BETWEEN_REQUESTS - (datetime.now() - last_request_time).total_seconds(), 0)
-        while has_ratelimit_error or wait:
-            if has_ratelimit_error:
+    while True:
+        print(DELAY_BETWEEN_REQUESTS)
+        api_key = api_keys_manager.get_next_key()
+        openai.api_key = api_key.key
+
+        wait = max(DELAY_BETWEEN_REQUESTS - (datetime.now() - api_key.last_request_time).total_seconds(), 0)
+        while api_key.has_ratelimit_error or wait:
+            if api_key.has_ratelimit_error:
                 await asyncio.sleep(3)
-                has_ratelimit_error = False
+                api_key.has_ratelimit_error = False
             await asyncio.sleep(wait)
-            wait = max(DELAY_BETWEEN_REQUESTS - (datetime.now() - last_request_time).total_seconds(), 0)
+            wait = max(DELAY_BETWEEN_REQUESTS - (datetime.now() - api_key.last_request_time).total_seconds(), 0)
 
         task = await queue.get()
         item = task["item"]
         task_id = task["task_id"]
-        await send_message_to_openai(item.request, task_id, item.max_waiting_time)
+        api_key.last_request_time = datetime.now()
+        await send_message_to_openai(item.request, task_id, item.max_waiting_time, api_key)
         
         if queue.empty():
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.01)
--- test.py
+++ test.py
@@ -1,36 +1,56 @@
 import requests
 import time
-import json
+import numpy as np
 
 BASE_URL = "http://localhost:8088"
 
-total_requests = 100
+total_requests = 3000
+
+
+prompts = [
+    "Describe a moment when you felt incredibly proud of someone else. How did their achievement affect your relationship with them?",
+    "Imagine you wake up in a post-apocalyptic world. What's your first move?",
+    "Write a letter to your future self 10 years from now. What advice would you give?",
+    "You are the inventor of a new gadget that can communicate with animals. Describe your first conversation with an animal and its implications.",
+    "Describe the perfect day in your dream city. What activities would you do? What sights would you see?",
+    "You have been chosen to be the ambassador of Earth to a newly discovered alien civilization. Write your first speech to the extraterrestrial beings.",
+    "Narrate a story where the protagonist is a sentient AI navigating human society.",
+    "Imagine you are a time traveler from the year 2200. Describe the most surprising change you see in the world of 2023.",
+    "You've found a hidden door in your house leading to a secret room. What's inside it?",
+    "The world now runs on a barter system instead of money. How does this change your daily life?",
+]
+
 
 request_dict = {
     "model": "gpt-3.5-turbo",
-    "messages": [
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Write 10 words article on this theme: 'Paris'"}
-    ]
 }
 
 start_time = time.time()
 
 task_ids = []
 for _ in range(total_requests):
-    response = requests.post(f"{BASE_URL}/task/", json={"request": request_dict})
+    request_dict["messages"] = [
+        {"role": "system", "content": "You are a helpful assistant. You answer is 10 words long maximum"},
+        {"role": "user", "content": np.random.choice(prompts)}
+    ]
+    response = requests.post(f"{BASE_URL}/task/", json={
+        "request": request_dict,
+        "max_waiting_time": 5
+    })
     response_data = response.json()
     task_ids.append(response_data['task_id'])
 
+counter = 0
 while task_ids:
     for task_id in task_ids:
         response = requests.get(f"{BASE_URL}/task/{task_id}")
         response_data = response.json()
         if response_data['status'] == 'ready':
-            print(response_data["result"].replace("\n", ""))
             task_ids.remove(task_id)
+            counter += 1
             break
-        time.sleep(0.001)  # Wait for 0.5 seconds before checking again
+        print(counter / (time.time() - start_time))
+        time.sleep(0.001) 
     # print(len(task_ids))
 
 end_time = time.time()
